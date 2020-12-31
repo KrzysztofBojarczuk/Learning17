cbc2c1d842f2689df0ba5f3376f96ec5ec15aab2
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2020-12-31 15:13:06 +0100

    Create FindPrime.java

---
+++ Java-KP/FindPrime.java
@@ -0,0 +1,17 @@
+public class FindPrime
+{
+    public static void main(String[] args) {
+        int num;
+        boolean isPrime = true;
+
+        num = 14;
+        for(int i=2; i<= num/i; i++) {
+            if((num % i) ==0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) System.out.println("Jest to liczba pierwsza");
+        else System.out.println("Nie jest to lcizba pierwsza");
+    }
+}

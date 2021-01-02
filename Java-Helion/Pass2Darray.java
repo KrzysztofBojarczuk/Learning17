cac488a0ccf0ceb025d4a62aab2679b65fd9e6d5
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-02 10:57:17 +0100

    Create Pass2Darray.java

---
+++ Java-Helion/Pass2Darray.java
@@ -0,0 +1,37 @@
+public class Pass2Darray
+{
+    public static void main(String[] args)
+    {
+        int[][] numbers = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
+
+        System.out.println("Oto wartość " + "zapisane w tablicy:");
+
+        showArray(numbers);
+
+        System.out.println("Suma wartość " + "wynosi " + arraySum(numbers) + ".");
+    }
+
+    private static void showArray(int[][] array)
+    {
+        for (int row = 0; row < array.length; row++)
+        {
+            for (int col = 0; col < array[row].length; col++)
+                System.out.print(array[row][col] + " ");
+            System.out.println();
+        }
+    }
+
+    private static int arraySum(int[][] array)
+    {
+        int total = 0;
+
+        for (int row = 0; row < array.length; row++)
+        {
+            for (int col = 0; col < array[row].length; col++)
+                total += array[row][col];
+        }
+        return total;
+    }
+}

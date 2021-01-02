4d2e1eff12cd34040a9271747350eb265c946792
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-02 10:42:28 +0100

    Create Lengths.java

---
+++ Java-Helion/Lengths.java
@@ -0,0 +1,15 @@
+public class Lengths
+{
+    public static void main(String[] args) {
+        int[][] numbers = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
+
+        System.out.println("Liczba wierszy " + "wynosi " + numbers.length + ".");
+
+        for (int index = 0; index < numbers.length; index++)
+        {
+            System.out.println("Liczba kolumn " + "w wierszu numer " + index + " wynosi " + numbers[index].length + ".");
+        }
+    }
+}

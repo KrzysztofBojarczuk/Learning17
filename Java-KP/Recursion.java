911958713d24cf07776f846137a29ba9969fd56e
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-02 14:50:23 +0100

    Create Recursion.java

---
+++ Java-KP/Recursion.java
@@ -0,0 +1,19 @@
+class Factorial{
+    int fact(int n){
+        int result;
+        if(n==1) return 1;
+        result = fact(n-1) *n;
+        return result;
+    }
+}
+
+public class Recursion
+{
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+
+        System.out.println("3! wynosi " + f.fact(3));
+        System.out.println("4! wynosi " + f.fact(4));
+        System.out.println("5! wynosi " + f.fact(5));
+    }
+}

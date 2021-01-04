f11009927c6be46ff898dc2ecd4f69922c5eb446
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-04 14:48:02 +0100

    Create StringDemo3.java

---
+++ Java-KP/StringDemo3.java
@@ -0,0 +1,9 @@
+public class StringDemo3
+{
+    public static void main(String[] args) {
+        String str[] = {"jeden", "dwa", "trzy"};
+
+        for(int i=0; i<str.length; i++)
+            System.out.println("str[" + i + "]:+ " + str[i]);
+    }
+}

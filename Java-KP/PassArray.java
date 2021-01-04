23807c931526385e13e8afb16bddbbd001a11085
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-04 15:43:59 +0100

    Create PassArray.java

---
+++ Java-KP/PassArray.java
@@ -0,0 +1,20 @@
+public class PassArray
+{
+    public static void vaTest(int v[]) {
+        System.out.print("Liczba argumentów: " + v.length + " Zawartość: ");
+
+        for(int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        int n1[] = { 10 };
+        int n2[] = { 1, 2, 3};
+        int n3[] = {};
+
+        vaTest(n1);
+        vaTest(n2);
+        vaTest(n3);
+    }
+}

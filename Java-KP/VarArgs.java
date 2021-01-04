5edcfcfbd82ce495d17116effde560598e788009
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-04 15:48:07 +0100

    Create VarArgs.java

---
+++ Java-KP/VarArgs.java
@@ -0,0 +1,19 @@
+public class VarArgs
+{
+    static void vaTest(int ... v)
+    {
+        System.out.print("Liczba arguemntów: " + v.length + " Zawartość: ");
+
+        for(int x : v)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        vaTest(10);
+        vaTest(1, 2, 3);
+        vaTest();
+    }
+}

5aa75bb05495a73855ec7b77cd0e963ed0731d4d
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-05 09:50:00 +0100

    Create DemoBoxWeight.java

---
+++ Java-KP/Basics of inheritance/DemoBoxWeight.java
@@ -0,0 +1,18 @@
+public class DemoBoxWeight
+{
+    public static void main(String[] args) {
+        BoxWeight mybox1 = new BoxWeight(10, 20, 15, 34.3);
+        BoxWeight mybox2 = new BoxWeight(2, 3, 4, 0.076);
+        double vol;
+
+        vol = mybox1.volumne();
+        System.out.println("Objętość mybox1: " + vol);
+        System.out.println("Ciężar mybox1: " + mybox1.weight);
+        System.out.println();
+
+        vol = mybox1.volumne();
+        System.out.println("Objętość mybox2: " + vol);
+        System.out.println("Ciężar mybox2: " + mybox2.weight);
+
+    }
+}

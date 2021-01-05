36a7d8469dcefce98ddd9452cdf681b4c34e8e08
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-05 07:27:18 +0100

    Create SimpleInheritance.java

---
+++ Java-KP/Basics of inheritance/SimpleInheritance.java
@@ -0,0 +1,25 @@
+public class SimpleInheritance
+{
+    public static void main(String[] args) {
+        A superOb = new A();
+        B subOb = new B();
+
+        superOb. i = 10;
+        superOb. j = 20;
+        System.out.println("Zawartość superOb:");
+        superOb.showij();
+        System.out.println();
+
+        subOb. i = 7;
+        subOb. j = 8;
+        subOb. k = 9;
+        System.out.println("Zawartość subOb: ");
+        subOb.showij();
+        subOb.showk();
+        System.out.println();
+
+        System.out.println("Suma i, j oraz k w subOb:");
+        subOb.sum();
+
+    }
+}
